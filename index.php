89c5ed2f2dac4fd8e3943c28ecaed8cc64aaa1b4
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-10 19:24:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $db_port = '3306';
 $db_name = 'db_1820595';
 
 $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
-$q = "SELECT * FROM accounts";
+$q = "SELECT * FROM tbl_device";
 $r = mysqli_query($conn, $q);
 
 echo "<?xml version='1.0' encoding='UTF-8'?>
@@ -16,10 +16,10 @@ echo "<?xml version='1.0' encoding='UTF-8'?>
 
 while ($item = mysqli_fetch_object($r)) {
     echo "<item>
-    <username>$item->username</username>
-    <email>$item->email</email>
-    <address>$item->address</address>
-    <contacts>$item->contacts</contacts>
+    <username>$item->brand</username>
+    <email>$item->cost</email>
+    <address>$item->year</address>
+    <contacts>$item->color</contacts>
     </item>";
 }
 
