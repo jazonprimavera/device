2b31b94d18250a726f35c292476e0b8b53ea3e01
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-10 19:15:29 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-type: text/xml");
+
+$db_username = 'admin';
+$db_password = 'root1234';
+$db_hostname = 'dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com';
+$db_port = '3306';
+$db_name = 'db_1820595';
+
+$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+$q = "SELECT * FROM accounts";
+$r = mysqli_query($conn, $q);
+
+echo "<?xml version='1.0' encoding='UTF-8'?>
+<data>";
+
+while ($item = mysqli_fetch_object($r)) {
+    echo "<item>
+    <username>$item->username</username>
+    <email>$item->email</email>
+    <address>$item->address</address>
+    <contacts>$item->contacts</contacts>
+    </item>";
+}
+
+echo "</data>";
