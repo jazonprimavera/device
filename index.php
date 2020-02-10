8a45ee26de4efb1dd9420df348949dc746ce2dec
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-10 17:32:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://kitarvin23.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("device");
+?>
+
+ <h1>Gadgets</h1>
+
+ <?php
+ foreach( $content as $data )
+ {?>
+     <div class="border">
+     <?php
+     $brand = $data->getElementsByTagName("brand")->item(0)->nodeValue;
+     $cost = $data->getElementsByTagName("cost")->item(0)->nodeValue;
+     $year = $data->getElementsByTagName("year")->item(0)->nodeValue;
+     $color = $data->getElementsByTagName("color")->item(0)->nodeValue;
+ 
+ echo "<ul>
+            <h2>$brand</h2>
+              <ul>
+                  <li>Cost: $cost </li>
+                  <li>Year: $year </li>
+                  <li>Album: $color </li>
+              </ul>
+          </ul>";
+    ?>
+     </div>
+  <?php
+ }
+?>
+</div>
+</div>
