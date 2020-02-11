15689b5d2731d5aea3262dbcfaecd3369cc5bcbf
jazonprimavera <60873633+jazonprimavera@users.noreply.github.com>
2020-02-11 09:59:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
  $domOBJ = new DOMDocument();
- $domOBJ->load("https://firstappanj.herokuapp.com//rss.php");//XML page URL
+ $domOBJ->load("https://devicedata.herokuapp.com//rss.php");//XML page URL
  
- $content = $domOBJ->getElementsByTagName("Books");
+ $content = $domOBJ->getElementsByTagName("device");
 ?>
 
  <h1>Books</h1>
@@ -12,16 +12,16 @@
  {?>
      <div class="border">
      <?php
-     $Title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-     $Author = $data->getElementsByTagName("author")->item(0)->nodeValue;
-     $Genre = $data->getElementsByTagName("genre")->item(0)->nodeValue;
-     $Date = $data->getElementsByTagName("date")->item(0)->nodeValue;
+     $brand = $data->getElementsByTagName("brand")->item(0)->nodeValue;
+     $cost = $data->getElementsByTagName("cost")->item(0)->nodeValue;
+     $year = $data->getElementsByTagName("year")->item(0)->nodeValue;
+     $color = $data->getElementsByTagName("color")->item(0)->nodeValue;
      echo "<ul>
-            <h2>$Title</h2>
+            <h2>$brand</h2>
               <ul>
-                  <li>Author: $Author </li>
-                  <li>Genre: $Genre </li>
-                  <li>Date Published: $Date </li>
+                  <li>cost: $cost </li>
+                  <li>year released: $year </li>
+                  <li>color: $color </li>
               </ul>
           </ul>";
     ?>
